a87be3336b0a2414c4c735689e71394f5cd60afb
piyapakkawatpakt-debug <piyapakkawat.pakt@bumail.net>
2026-08-18 16:22:21 +0700

    Workshop-code

--- Assets/Scripts/Workshop.cs
+++ Assets/Scripts/Workshop.cs
@@ -6,11 +6,11 @@ public class Workshop : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // As01_SyntaxIf();
-        // As02_StringComparisonExample();
-        // As03_NumberComparisonExample();
-        // As04_AndOrOperatorExample();
-        // As05_GuessingNumberExample();
+         //As01_SyntaxIf();
+        //As02_StringComparisonExample();
+         //As03_NumberComparisonExample();
+        //As04_AndOrOperatorExample();
+         As05_GuessingNumberExample();
         // As06_GuessingNumberMoreOrLessExample();
         // As07_VerifyIdentityExample();
     }
@@ -18,39 +18,91 @@ public class Workshop : MonoBehaviour
     public bool isSixOClock;
     public void As01_SyntaxIf()
     {
-        
+        if (isSixOClock)
+        {
+            Debug.Log("The door open");
+        }
+        Debug.Log("Knock Knock!");
     }
 
     public string password;
     public void As02_StringComparisonExample()
     {
-        
+        if(password == "Moon")
+        {
+            Debug.Log("Password is correct");
+        }
+        if (password != "Moon")
+        {
+            Debug.Log("Password is Not correct");
+        }
     }
 
     public int as03Number;
     public void As03_NumberComparisonExample()
     {
+        if (as03Number > 10)
+        {
+            Debug.Log("My number > 10");
+        }
+        if (as03Number >= 10)
+        {
+            Debug.Log("My number >= 10");
+        }
+        if (as03Number < 10)
+        {
+            Debug.Log("My number < 10");
+        }
+        if (as03Number <= 10)
+        {
+            Debug.Log("My number <= 10");
+        }
+        if (as03Number == 10)
+        {
+            Debug.Log("My number == 10");
+        }
+        if (as03Number != 10)
+        {
+            Debug.Log("My number != 10");
+        }
 
     }
 
     public int as04Number;
     public void As04_AndOrOperatorExample()
     {
-
+        if (as04Number > 8 && as04Number < 12)
+        {
+            Debug.Log("My number 8 > <12");
+        }
+        if (as04Number < 8 || as04Number > 12)
+        {
+            Debug.Log("My number 8 || 12");
+        }
     }
 
     public int as05GuessingNumber;
     public int as05RandomNumber;
     public void As05_GuessingNumberExample()
     {
-
+        if (as05GuessingNumber == as05RandomNumber) {
+            Debug.Log("Congratulation");
+        }
+        else
+        {
+            Debug.Log("Try agin");
+        }
     }
 
     public int as06GuessingNumber;
     public int as06RandomNumber;
     public void As06_GuessingNumberMoreOrLessExample()
     {
-
+        if (as06GuessingNumber < as06RandomNumber)
+        {
+            Debug.Log("TooLow");
+        }
+        
     }
 
     public string as07Username;
